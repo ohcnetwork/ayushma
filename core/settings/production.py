1cae55030cfcc4b695adf6665a1618b6c6dd08b1
skks1212 <kacker.shivank@gmail.com>
2023-04-12 09:40:22 +0530

    added csrf trusted oriins

--- core/settings/production.py
+++ core/settings/production.py
@@ -14,7 +14,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[""])
+CSRF_TRUSTED_ORIGINS = env.list("DJANGO_CSRF_TRUSTED_ORIGINS", default=[""])
 
 # DATABASES
 # ------------------------------------------------------------------------------
