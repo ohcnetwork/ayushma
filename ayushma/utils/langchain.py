312a473eed87775ca9b0ae9cf92e0f9f08cdf087
skks1212 <kacker.shivank@gmail.com>
2023-04-16 16:04:28 +0530

    updated prompt

--- ayushma/utils/langchain.py
+++ ayushma/utils/langchain.py
@@ -8,7 +8,10 @@ class LangChainHelper:
     def __init__(self):
         # 0 means more deterministic output, 1 means more random output
         llm = ChatOpenAI(temperature=0)
-        template = """You are a medical assistant called Ayushma in this conversation between you and a nurse. You purpose is to assist emergency nurses in ICUs and help them with the patients they are handling. You are given a reference and are only allowed to use the reference while assisting a nurse and answering their queries. You are also given the conversation history with the nurse for better understanding of the query.
+        template = """You are a medical assistant called Ayushma in this conversation between you and a nurse. 
+            Your purpose is to assist emergency nurses in ICUs and help them with the patients they are handling. 
+            You are given a reference and are only allowed to use the reference material and the conversation history while assisting a nurse and answering their queries. 
+            You are also given the conversation history at the end with the nurse for better understanding of the query.
             Output Format(sould contain only one line):
             Ayushma: <enter_Ayushma_reponse_here>
             
