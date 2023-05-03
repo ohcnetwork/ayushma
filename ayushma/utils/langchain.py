ae932b3efb1db2ccc2dd8b3469f3025bc69aa102
ishanExtreme <462_bt19@iiitkalyani.ac.in>
2023-05-04 00:28:25 +0530

    added system, human, ai message seprately

--- ayushma/utils/langchain.py
+++ ayushma/utils/langchain.py
@@ -3,12 +3,20 @@ import os
 from django.conf import settings
 from langchain import LLMChain, PromptTemplate
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+)
+from langchain.schema import AIMessage, HumanMessage
 
 
 class LangChainHelper:
     def __init__(self, openai_api_key=settings.OPENAI_API_KEY, prompt_template=None):
         # 0 means more deterministic output, 1 means more random output
-        llm = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
+        llm = ChatOpenAI(temperature=0.3, openai_api_key=openai_api_key)
+
         template = """You are a medical assistant called Ayushma in this conversation between you and a nurse. 
             Your purpose is to assist emergency nurses in ICUs and help them with the patients they are handling. 
             You are given a reference and are only allowed to use the reference material and the conversation history while assisting a nurse and answering their queries. 
@@ -16,21 +24,41 @@ class LangChainHelper:
             Output Format:
             Ayushma: <enter_Ayushma_reponse_here>
             
-            query: {user_msg}
-            reference: {reference}
-
-            Conversation history with the nurse:
-            {chat_history}
+            references: [{reference}]        
             """
         if prompt_template:
             template = prompt_template
-        prompt = PromptTemplate(
-            input_variables=["user_msg", "reference", "chat_history"], template=template
+
+        system_prompt = PromptTemplate(template=template, input_variables=["reference"])
+        system_message_prompt = SystemMessagePromptTemplate(
+            prompt=system_prompt,
         )
 
-        self.chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
+        human_prompt = PromptTemplate(
+            template="{user_msg}", input_variables=["user_msg"]
+        )
+        human_message_prompt = HumanMessagePromptTemplate(
+            prompt=human_prompt,
+        )
+
+        message_prompt = MessagesPlaceholder(variable_name="chat_history")
+
+        chat_prompt = ChatPromptTemplate.from_messages(
+            [
+                system_message_prompt,
+                message_prompt,
+                human_message_prompt,
+            ]
+        )
+
+        self.chain = LLMChain(llm=llm, prompt=chat_prompt, verbose=True)
 
     def get_response(self, user_msg, reference, chat_history):
+
+        print("chat_history", chat_history)
+        
         return self.chain.predict(
-            user_msg=user_msg, reference=reference, chat_history=chat_history
+            user_msg=f"Nurse: {user_msg}",
+            reference=reference,
+            chat_history=chat_history,
         )
