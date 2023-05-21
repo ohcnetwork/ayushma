92d587a6a65ee7ab2797def838f22147fba12abb
siddnikh <mailtosiddnikh@gmail.com>
2023-05-21 14:59:02 +0530

    Added UserType enums for User model

--- ayushma/models/enums.py
+++ ayushma/models/enums.py
@@ -10,3 +10,9 @@ class DocumentType(IntegerChoices):
     FILE = 1
     URL = 2
     TEXT = 3
+
+# User is allowed only chat access, supervisor - can manage projects, admin - can manage everything (incl. users)
+class UserType(IntegerChoices):
+    USER = 1
+    SUPERVISOR = 2
+    ADMIN = 3
--- ayushma/models/users.py
+++ ayushma/models/users.py
@@ -4,12 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import UserManager as BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-def reverse_choices(choices):
-    output = {}
-    for choice in choices:
-        output[choice[0]] = choice[1]
-    return output
+from enums import UserType
 
 class UserManager(BaseUserManager):
     pass
@@ -24,18 +19,7 @@ class User(AbstractUser):
     full_name = models.CharField(max_length=80, blank=True)
     email = models.EmailField(_("email address"), unique=True, db_index=True)
 
-    # User is allowed only chat access, supervisor - can manage projects, admin - can manage everything (incl. users)
-    TYPE_VALUE_MAP = {
-        "User": 1,
-        "Supervisor": 2,
-        "Admin": 3
-    }
-
-    TYPE_CHOICES = [(value, name) for name, value in TYPE_VALUE_MAP.items()]
-
-    REVERSE_TYPE_MAP = reverse_choices(TYPE_CHOICES)
-
-    user_type = models.IntegerField(choices=TYPE_CHOICES, blank=False)
+    user_type = models.IntegerField(choices=UserType, blank=False, default=UserType.USER)
 
     last_login_platform = models.CharField(max_length=100, blank=True)
     last_login_ip = models.GenericIPAddressField(blank=True, null=True)
