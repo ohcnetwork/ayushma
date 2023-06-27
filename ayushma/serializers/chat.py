78f7caea058a40dcac4fbd274e13f13b5a82d8a5
khavinshankar <khavinshankar@gmail.com>
2023-06-27 17:46:19 +0530

    send chatmessage id to frontend

--- ayushma/serializers/chat.py
+++ ayushma/serializers/chat.py
@@ -49,6 +49,7 @@ class ChatMessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChatMessage
         fields = (
+            "id",
             "external_id",
             "chat",
             "messageType",
@@ -65,6 +66,7 @@ class ChatMessageSerializer(serializers.ModelSerializer):
             "feedback",
         )
         read_only_fields = (
+            "id",
             "external_id",
             "created_at",
             "modified_at",
