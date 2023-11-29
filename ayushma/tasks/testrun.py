4cb060b78086ba33491eeea50cadfda3a7c21902
Ashesh <3626859+Ashesh3@users.noreply.github.com>
2023-11-29 18:30:12 +0530

    handle assistant_id for test runs (#402)

--- ayushma/tasks/testrun.py
+++ ayushma/tasks/testrun.py
@@ -10,7 +10,12 @@ from ayushma.models.chat import Chat
 from ayushma.models.enums import StatusChoices
 from ayushma.models.testsuite import TestResult, TestRun
 from ayushma.utils.language_helpers import translate_text
-from ayushma.utils.openaiapi import converse, cosine_similarity, get_embedding
+from ayushma.utils.openaiapi import (
+    converse,
+    converse_thread,
+    cosine_similarity,
+    get_embedding,
+)
 
 
 @shared_task(bind=True, soft_time_limit=21600)  # 6 hours in seconds
@@ -59,26 +64,36 @@ def mark_test_run_as_completed(self, test_run_id):
                         test_question.language + "-IN", english_text
                     )
 
-                ai_response = next(
-                    converse(
+                if test_run.project.assistant_id:
+                    ai_response = converse_thread(
+                        thread=chat,
                         english_text=english_text,
-                        local_translated_text=translated_text,
                         openai_key=settings.OPENAI_API_KEY,
-                        chat=chat,
-                        match_number=topk,
-                        stats=dict(),
-                        temperature=temperature,
-                        user_language=test_question.language + "-IN",
-                        stream=False,
-                        generate_audio=False,
-                        fetch_references=test_run.references,
-                        documents=test_question.documents.all(),
                     )
-                )
+                    reference_documents = []
+                else:
+                    response = next(
+                        converse(
+                            english_text=english_text,
+                            local_translated_text=translated_text,
+                            openai_key=settings.OPENAI_API_KEY,
+                            chat=chat,
+                            match_number=topk,
+                            stats=dict(),
+                            temperature=temperature,
+                            user_language=test_question.language + "-IN",
+                            stream=False,
+                            generate_audio=False,
+                            fetch_references=test_run.references,
+                            documents=test_question.documents.all(),
+                        )
+                    )
+                    ai_response = response.message
+                    reference_documents = response.reference_documents
 
                 # Calculate cosine similarity
                 openai.api_key = settings.OPENAI_API_KEY
-                ai_response_embedding = get_embedding(ai_response.message)
+                ai_response_embedding = get_embedding(ai_response)
                 human_answer_embedding = get_embedding(test_question.human_answer)
                 cosine_sim = cosine_similarity(
                     ai_response_embedding, human_answer_embedding
@@ -86,18 +101,18 @@ def mark_test_run_as_completed(self, test_run_id):
 
                 # Calculate BLEU score ( https://www.nltk.org/api/nltk.translate.bleu_score.html#nltk.translate.bleu_score.SmoothingFunction.__init__ )
                 reference_tokens = test_question.human_answer.split()
-                candidate_tokens = ai_response.message.split()
+                candidate_tokens = ai_response.split()
 
                 smoothie = SmoothingFunction().method4
                 bleu_score = sentence_bleu(
                     [reference_tokens], candidate_tokens, smoothing_function=smoothie
                 )
 
-                test_result.answer = ai_response.message
+                test_result.answer = ai_response
                 test_result.cosine_sim = cosine_sim
                 test_result.bleu_score = round(bleu_score, 4)
                 try:
-                    test_result.references.set(ai_response.reference_documents.all())
+                    test_result.references.set(reference_documents.all())
                 except Exception:
                     pass
 
