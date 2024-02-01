c4cf73131d03ae27e801e7a7f4afb406117ecc75
Ashesh <3626859+Ashesh3@users.noreply.github.com>
2024-02-01 22:58:45 +0530

    Add check for empty recognized text in speech_to_text function (#451)

--- ayushma/utils/speech_to_text.py
+++ ayushma/utils/speech_to_text.py
@@ -91,7 +91,9 @@ def speech_to_text(engine_id, audio, language_code):
 
     try:
         engine = engine_class(api_key, language_code)
-        return engine.recognize(audio)
+        recognized_text = engine.recognize(audio)
+        if not recognized_text:
+            raise ValueError("Failed to detect any speech in provided audio")
     except Exception as e:
         print(f"Failed to recognize speech with {engine_name} engine: {e}")
         raise e
